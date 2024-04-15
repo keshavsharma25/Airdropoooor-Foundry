9965455f90c7315322b93aca9a170d94530107f1
keshavsharma25 <skeshav0825@gmail.com>
2024-04-16 03:29:09 +0530

    feat: add utility functions and remove abstract

--- packages/foundry/contracts/Account.sol
+++ packages/foundry/contracts/Account.sol
@@ -11,23 +11,44 @@ import {IERC6551Registry} from "erc6551/interfaces/IERC6551Registry.sol";
 
 import {IERC721A} from "erc721a/contracts/IERC721A.sol";
 
-abstract contract Account is ERC165, IERC1271, IERC6551Account {
+contract Account is ERC165, IERC1271, IERC6551Account {
+    /* ------------------------------- errors ------------------------------- */
+    error NotAuthorized();
+
+    /* -------------------------- state variables -------------------------- */
+    uint256 public state;
+
     /* ----------------------------- override ---------------------------- */
+
+    receive() external payable {}
 
     function supportsInterface(
         bytes4 interfaceId
-    ) public pure override returns (bool) {
-        return interfaceId == type(ERC165).interfaceId;
+    ) public pure override(ERC165) returns (bool) {
+        return
+            interfaceId == type(ERC165).interfaceId ||
+            interfaceId == type(IERC6551Account).interfaceId;
     }
 
     function _isValidSigner(address signer) internal view returns (bool) {
         return signer == 0x0000000000000000000000000000000000000000;
     }
 
+    function owner() public view returns (address) {
+        (uint256 chainId, address tokenContract, uint256 tokenId) = token();
+        if (chainId != block.chainid) return address(0);
+
+        return IERC721A(tokenContract).ownerOf(tokenId);
+    }
+
+    function token() public view returns (uint256, address, uint256) {
+        return ERC6551AccountLib.token();
+    }
+
     function isValidSigner(
         address signer,
         bytes calldata
-    ) external view virtual returns (bytes4) {
+    ) external view returns (bytes4) {
         if (_isValidSigner(signer)) {
             return IERC6551Account.isValidSigner.selector;
         }
@@ -37,7 +58,7 @@ abstract contract Account is ERC165, IERC1271, IERC6551Account {
     function isValidSignature(
         bytes32 hash,
         bytes memory signature
-    ) external view virtual returns (bytes4 magicValue) {
+    ) external view returns (bytes4 magicValue) {
         bool isValid = SignatureChecker.isValidSignatureNow(
             0x0000000000000000000000000000000000000000,
             hash,
