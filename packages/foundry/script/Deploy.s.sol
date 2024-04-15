c81a4bc0327c07c65b193611ee6f75d7ff28f9fb
keshavsharma25 <skeshav0825@gmail.com>
2024-04-16 01:36:33 +0530

    chore: remove YourContract

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "../contracts/YourContract.sol";
 import "./DeployHelpers.s.sol";
 
 contract DeployScript is ScaffoldETHDeploy {
@@ -15,13 +14,15 @@ contract DeployScript is ScaffoldETHDeploy {
             );
         }
         vm.startBroadcast(deployerPrivateKey);
-        YourContract yourContract =
+
+        /* YourContract yourContract =
             new YourContract(vm.addr(deployerPrivateKey));
         console.logString(
             string.concat(
                 "YourContract deployed at: ", vm.toString(address(yourContract))
             )
-        );
+        ); */
+
         vm.stopBroadcast();
 
         /**
