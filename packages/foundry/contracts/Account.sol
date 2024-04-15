090af62c3604dc1ac367c9a9450011b2611de3a7
keshavsharma25 <skeshav0825@gmail.com>
2024-04-16 01:40:09 +0530

    feat: create Account.sol for erc6551

---
+++ packages/foundry/contracts/Account.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import {IERC1271} from "@openzeppelin/contracts/interfaces/IERC1271.sol";
+import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import {IERC6551Account} from "erc6551/interfaces/IERC6551Account.sol";
+
+abstract contract Account is ERC165, IERC1271, IERC6551Account {
+    /* ----------------------------- override ---------------------------- */
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public pure override returns (bool) {
+        return interfaceId == type(ERC165).interfaceId;
+    }
+
+    function _isValidSigner(address signer) internal view returns (bool) {
+        return signer == 0x0000000000000000000000000000000000000000;
+    }
+
+    function isValidSigner(
+        address signer,
+        bytes calldata
+    ) external view virtual returns (bytes4) {
+        if (_isValidSigner(signer)) {
+            return IERC6551Account.isValidSigner.selector;
+        }
+
+        return bytes4(0);
+    }
+    function isValidSignature(
+        bytes32 hash,
+        bytes memory signature
+    ) external view virtual returns (bytes4 magicValue) {
+        bool isValid = SignatureChecker.isValidSignatureNow(
+            0x0000000000000000000000000000000000000000,
+            hash,
+            signature
+        );
+
+        if (isValid) {
+            return IERC1271.isValidSignature.selector;
+        }
+
+        return bytes4(0);
+    }
+}
