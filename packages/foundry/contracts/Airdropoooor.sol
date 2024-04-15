fe13717d99f2ebb26948b7a81cd9615daf18192a
keshavsharma25 <skeshav0825@gmail.com>
2024-04-16 03:31:37 +0530

    feat: add getAbbrvTBA, tokenURI

--- packages/foundry/contracts/Airdropoooor.sol
+++ packages/foundry/contracts/Airdropoooor.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.20;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 import {IERC4907} from "./interfaces/IERC4907.sol";
 import {ERC721A} from "erc721a/contracts/ERC721A.sol";
 
@@ -35,7 +36,6 @@ contract Airdropoooor is ERC721A, IERC4907, Ownable {
         _safeMint(_maintainer, _noAirdrops);
     }
 
-
     /* ----------------------------- public ----------------------------- */
 
     function setUser(uint256 tokenId, address user, uint64 expires) public {
@@ -57,12 +57,38 @@ contract Airdropoooor is ERC721A, IERC4907, Ownable {
         }
     }
 
-    function userExpires(uint256 tokenId) public view returns (uint256) {
-        return _users[tokenId].expires;
+    function userExpires(uint256 _tokenId) public view returns (uint256) {
+        return _users[_tokenId].expires;
     }
 
-    function tokenURI(uint256 _tokenId) public view override returns(string memory) {}
+    function getAbbrvTBA(
+        uint256 _tokenId
+    ) public view returns (string memory) {}
 
+    function tokenURI(
+        uint256 _tokenId
+    ) public view override returns (string memory) {
+        return
+            string.concat(
+                '<svg width="400" height="400" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">',
+                '<rect width="400" height="400" rx="40" fill="url(#a)"/>',
+                '<circle cx="199.5" cy="131.5" r="81.5" stroke="#130912" stroke-width="2"/>',
+                '<text fill="#130912" xml:space="preserve" style="white-space:pre" font-family="Noto Sans" font-size="36" letter-spacing="0em"><tspan x="149" y="121.468">MOCK</tspan><tspan x="149" y="170.468">ERC20</tspan></text>',
+                '<text fill="#130912" xml:space="preserve" style="white-space:pre" font-family="Noto Sans" font-size="20" letter-spacing="0em"><tspan x="150" y="272.26">TokenID : ',
+                Strings.toString(_tokenId),
+                "</tspan></text>",
+                '<text fill="#130912" xml:space="preserve" style="white-space:pre" font-family="Noto Sans" font-size="20" letter-spacing="0em"><tspan x="117" y="302.26">TBA : ',
+                getAbbrvTBA(_tokenId),
+                "</tspan></text>",
+                "<defs>",
+                '<linearGradient id="a" x1="-62.5" y1="-57" x2="414" y2="422" gradientUnits="userSpaceOnUse">',
+                '<stop stop-color="#8386FD"/>',
+                '<stop offset="1" stop-color="#E192A7"/>',
+                "</linearGradient>",
+                "</defs>",
+                "</svg>"
+            );
+    }
 
     /* ----------------------------- internal ---------------------------- */
 
