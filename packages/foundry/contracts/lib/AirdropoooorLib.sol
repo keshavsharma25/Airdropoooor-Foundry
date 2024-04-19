cc4e741fd20047970202f3f2f97bd8054f2b4421
keshavsharma25 <skeshav0825@gmail.com>
2024-04-19 20:07:34 +0530

    feat: add isClaimed & isWithdrawn to tbaToAirdrop Struct

--- packages/foundry/contracts/lib/AirdropoooorLib.sol
+++ packages/foundry/contracts/lib/AirdropoooorLib.sol
@@ -1,16 +1,21 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/* ######################################################################### */
+/*                          library: AirdropoooorLib                         */
+/* ######################################################################### */
 library AirdropoooorLib {
     struct ToAirdrop {
         address toAddress;
         uint256 amount;
     }
 
-    struct tbaToAirdrop {
-        address toaddress;
+    struct TbaToAirdrop {
+        address toAddress;
         address tbaAddress;
         uint256 amount;
+        bool isClaimed;
+        bool isWithdrawn;
     }
 
     enum AirdropStatus {
