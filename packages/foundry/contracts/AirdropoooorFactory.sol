df7973878f01a4fab21398571aa4e02501b52aed
keshavsharma25 <skeshav0825@gmail.com>
2024-04-19 20:23:53 +0530

    feat: add event, setup maintainer & airdropoooor in createAirdropoooor function

--- packages/foundry/contracts/AirdropoooorFactory.sol
+++ packages/foundry/contracts/AirdropoooorFactory.sol
@@ -2,36 +2,63 @@
 pragma solidity ^0.8.20;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {AirdropoooorLib as adLib} from "./lib/AirdropoooorLib.sol";
+import {AirdropoooorLib as adl} from "./lib/AirdropoooorLib.sol";
 import {Maintainer} from "./Maintainer.sol";
 import {Airdropoooor} from "./Airdropoooor.sol";
 
+/* ######################################################################### */
+/*                            AirdropoooorFactory                            */
+/* ######################################################################### */
 contract AirdropoooorFactory is Ownable {
+    /* ------------------------------- events ------------------------------ */
+    event CreateAirdropoooor(
+        address indexed airdropoooor,
+        address maintainer,
+        address tokenAddress
+    );
+
+    /* -------------------------- state variables -------------------------- */
+    address public immutable REGISTRY_ADDRESS;
     mapping(address => address) private _tokenAirdropMapper;
 
-    constructor(address _owner) Ownable(_owner) {}
+    /* ---------------------------- constructor ---------------------------- */
+    constructor(address _owner, address _registry) Ownable(_owner) {
+        REGISTRY_ADDRESS = _registry;
+    }
 
-    function createAirDropoooor(
+    /* ------------------------------- public ------------------------------ */
+    function createAirdropoooor(
         string calldata _name,
         string calldata _symbol,
-        string calldata _logo,
         address _airdropTokenAddr,
         address _admin,
-        adLib.ToAirdrop[] calldata _airdropInfo,
+        adl.ToAirdrop[] memory _airdropInfo,
         uint256 _deadlineTimestamp
     ) public payable {
-        Maintainer maintainer = new Maintainer(_admin, _airdropTokenAddr);
+        Maintainer maintainer = new Maintainer(
+            _admin,
+            _airdropTokenAddr,
+            _airdropInfo,
+            _deadlineTimestamp,
+            REGISTRY_ADDRESS,
+            address(this)
+        );
 
         Airdropoooor airdrop = new Airdropoooor(
             _name,
             _symbol,
-            _logo,
             _admin,
             address(maintainer),
-            _deadlineTimestamp,
             _airdropInfo.length
         );
 
+        maintainer.setAirdropoooorAddress(address(airdrop));
         _tokenAirdropMapper[address(airdrop)] = _airdropTokenAddr;
+
+        emit CreateAirdropoooor(
+            address(airdrop),
+            address(maintainer),
+            _airdropTokenAddr
+        );
     }
 }
