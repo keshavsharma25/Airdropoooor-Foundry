705986e9d589a7e443ec06296c2e41f21597b307
keshavsharma25 <skeshav0825@gmail.com>
2024-05-07 09:54:43 +0530

    update token URI

--- packages/foundry/contracts/Airdropoooor.sol
+++ packages/foundry/contracts/Airdropoooor.sol
@@ -95,24 +95,59 @@ contract Airdropoooor is ERC4907A, ERC721ABurnable, Ownable {
         uint256 _tokenId
     ) public view override(ERC721A, IERC721A) returns (string memory) {
         return
-            string.concat(
-                '<svg width="400" height="400" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">',
-                '<rect width="400" height="400" rx="40" fill="url(#a)"/>',
-                '<circle cx="199.5" cy="131.5" r="81.5" stroke="#130912" stroke-width="2"/>',
-                '<text fill="#130912" xml:space="preserve" style="white-space:pre" font-family="Noto Sans" font-size="36" letter-spacing="0em"><tspan x="149" y="121.468">MOCK</tspan><tspan x="149" y="170.468">ERC20</tspan></text>',
-                '<text fill="#130912" xml:space="preserve" style="white-space:pre" font-family="Noto Sans" font-size="20" letter-spacing="0em"><tspan x="150" y="272.26">TokenID : ',
-                Strings.toString(_tokenId),
-                "</tspan></text>",
-                '<text fill="#130912" xml:space="preserve" style="white-space:pre" font-family="Noto Sans" font-size="20" letter-spacing="0em"><tspan x="117" y="302.26">TBA : ',
-                getAbbrvTBA(_tokenId),
-                "</tspan></text>",
-                "<defs>",
-                '<linearGradient id="a" x1="-62.5" y1="-57" x2="414" y2="422" gradientUnits="userSpaceOnUse">',
-                '<stop stop-color="#8386FD"/>',
-                '<stop offset="1" stop-color="#E192A7"/>',
-                "</linearGradient>",
-                "</defs>",
-                "</svg>"
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(
+                        bytes(
+                            string.concat(
+                                '{ "name": "',
+                                name(),
+                                '", "image": "data:image/svg+xml;base64,',
+                                Base64.encode(bytes(_tokenSVG(_tokenId))),
+                                '", "description": "Airdropoooor: claim your airdrops with ease"}'
+                            )
+                        )
+                    )
+                )
             );
+    }
+
+    function _tokenSVG(uint256 _tokenId) internal view returns (string memory) {
+        bool isClaimed = IMaintainer(payable(MAINTAINER_ADDRESS)).isClaimed(
+            _tokenId
+        );
+
+        string memory svg = string.concat(
+            '<svg width="400" height="400" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">',
+            '<rect width="400" height="400" rx="40" fill="url(#paint0_linear_1734_831)"/>',
+            '<text fill="#130912" xml:space="preserve" style="white-space: pre" font-family="Noto Sans" font-size="20" letter-spacing="0em"><tspan x="150" y="272.26">TokenID : ',
+            Strings.toString(_tokenId),
+            "</tspan></text>",
+            '<text fill="#130912" xml:space="preserve" style="white-space: pre" font-family="Noto Sans" font-size="36" letter-spacing="0em"><tspan x="149" y="121.468">',
+            name(),
+            "</tspan>"
+        );
+
+        if (isClaimed) {
+            svg = string.concat(
+                svg,
+                '<rect y="322" width="400" height="34" fill="#D9D9D9"/>',
+                '<text fill="#130912" xml:space="preserve" style="white-space: pre" font-family="Inter" font-size="16" letter-spacing="0em"><tspan x="159" y="344.318">Redeemed</tspan></text>'
+            );
+        }
+
+        svg = string.concat(
+            svg,
+            "<defs>",
+            '<linearGradient id="paint0_linear_1734_831" x1="-62.5" y1="-57" x2="414" y2="422" gradientUnits="userSpaceOnUse">',
+            '<stop stop-color="#8386FD"/>',
+            '<stop offset="1" stop-color="#E192A7"/>',
+            "</linearGradient>",
+            "</defs>",
+            "</svg>"
+        );
+
+        return svg;
     }
 }
